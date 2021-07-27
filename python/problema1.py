cf76a064cbb15c2faec4e911d944ead5cdf0d929
israel palma <ipalm001@gmail.com>
2021-07-27 14:35:36 -0400

    problema1

--- python/problema1.py
+++ python/problema1.py
@@ -2,3 +2,11 @@
 # nombre, apellido, edad, sexo, estatura, peso, email
 # validar el ingreso
 
+edad = input("Ingrese edad: ")
+if edad =="":
+    print ("Error: Ingrese edad")
+else:
+    if edad >= "18":
+        print ("Puede pasar")
+    else:
+        print("No puede pasar")
